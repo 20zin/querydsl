c50d0c919fec5353320535c57aa6631f6a658e00
YoungjinLee <117518123+20zin@users.noreply.github.com>
2024-03-16 20:38:32 +0900

    4 commit

---
+++ src/test/java/com/example/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.querydsl.repository;
+
+import com.example.querydsl.entity.Member;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.swing.text.html.parser.Entity;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest(){
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+    }
+
+}
